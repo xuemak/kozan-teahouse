c7afb1a281b68ed018841a3901e459691f19e4be
Nicole Mak <nicolexuemak@gmail.com>
2025-04-08 10:48:36 -0700

    Added React. Removed predefined CSS. Created Navbar component. Added Navbar component to page.tsx file.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="Nav">
+            <ul>
+                <li>Milk Tea</li>
+                <li>Kozan Shakes</li>
+                <li>Refreshers</li>
+                <li>Oreo Brulee</li>
+                <li>Matcha</li>
+                <li>More</li>
+            </ul>
+        </nav>
+    )
+}
