fc5365e4584601ef6a6df67322dbcd6f391bf525
Nicole Mak <nicolexuemak@gmail.com>
2025-04-09 11:32:04 -0700

    Added logo to public folder. Added basic styling to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,37 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <nav className="Nav">
-            <ul>
-                <li>Milk Tea</li>
-                <li>Kozan Shakes</li>
-                <li>Refreshers</li>
-                <li>Oreo Brulee</li>
-                <li>Matcha</li>
-                <li>More</li>
-            </ul>
+        <nav className="bg-purple-200 fixed w-full">
+            <div className="flex flex-wrap items-center justify-center p-4">
+            <div>
+                <ul className="flex p-0 font-medium bg-purple-200 space-x-8">
+                    <li>
+                        <a href="#">Milk Tea</a>
+                    </li>
+                    <li>
+                        <a href="#">Smoothies</a>
+                    </li>
+                    <li>
+                        <a href="#">Refreshers</a>
+                    </li>
+                    <li>
+                        <a href="#">Oreo Brûlée</a>
+                    </li>
+                    <li>
+                        <a href="#">Matcha</a>
+                    </li>
+                    <li>
+                        <a href="#">Coffee</a>
+                    </li>
+                    <li>
+                        <a href="#">Milk</a>
+                    </li>
+                    <li>
+                        <a href="#">Cheese Foam</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
         </nav>
     )
 }
