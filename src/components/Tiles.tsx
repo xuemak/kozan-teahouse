97636fc793800edbed56c26fed0aef11c8ed05fe
Nicole Mak <nicolexuemak@gmail.com>
2025-04-10 16:25:56 -0700

    Added drink details for the first drink, Brown Sugar Milk Tea

--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -3,7 +3,21 @@ import Image from "next/image";
 
 export default function Tiles() {
     return (
-        <div className="grid grid-cols-4 gap-10 p-50">
+        <div className="grid grid-cols-4 gap-6 p-50">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <Image
+                src="/images/drinks/bs-mt.webp"
+                width={234}
+                height={234}
+                className="w-full"
+                alt="Brown Sugar Milk Tea"/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-l mb-2 flex items-center justify-center">Brown Sugar Milk Tea</div>
+                <p className="text-gray-700 text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+            </div>
+            </div>
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
             <Image
                 src="/images/drinks/bs-mt.webp"
