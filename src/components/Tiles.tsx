30a04c179737325261d4acaad8cf71fb5bf82328
Nicole Mak <nicolexuemak@gmail.com>
2025-04-11 09:24:43 -0700

    Applied uniform styling to all card components for the milk tea section

--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -28,10 +28,13 @@ export default function Tiles() {
                 height={234}
                 className="w-full"
                 alt="Brown Sugar Milk Tea"/>
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            <div className="px-3 py-7 bg-orange-100">
+                <div className="font-black text-base mb-2 text-center uppercase">Brown Sugar Milk Tea</div>
+                <p className="text-stone-500 text-base text-center font-light text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+                <p className="text-gray-700 text-base text-center p-4">
+                    $6.25
                 </p>
             </div>
             </div>
@@ -42,10 +45,13 @@ export default function Tiles() {
                 height={234}
                 className="w-full"
                 alt="Brown Sugar Milk Tea"/>
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            <div className="px-3 py-7 bg-orange-100">
+                <div className="font-black text-base mb-2 text-center uppercase">Brown Sugar Milk Tea</div>
+                <p className="text-stone-500 text-base text-center font-light text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+                <p className="text-gray-700 text-base text-center p-4">
+                    $6.25
                 </p>
             </div>
             </div>
@@ -56,10 +62,13 @@ export default function Tiles() {
                 height={234}
                 className="w-full"
                 alt="Brown Sugar Milk Tea"/>
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            <div className="px-3 py-7 bg-orange-100">
+                <div className="font-black text-base mb-2 text-center uppercase">Brown Sugar Milk Tea</div>
+                <p className="text-stone-500 text-base text-center font-light text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+                <p className="text-gray-700 text-base text-center p-4">
+                    $6.25
                 </p>
             </div>
             </div>
@@ -70,10 +79,13 @@ export default function Tiles() {
                 height={234}
                 className="w-full"
                 alt="Brown Sugar Milk Tea"/>
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            <div className="px-3 py-7 bg-orange-100">
+                <div className="font-black text-base mb-2 text-center uppercase">Brown Sugar Milk Tea</div>
+                <p className="text-stone-500 text-base text-center font-light text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+                <p className="text-gray-700 text-base text-center p-4">
+                    $6.25
                 </p>
             </div>
             </div>
@@ -84,10 +96,13 @@ export default function Tiles() {
                 height={234}
                 className="w-full"
                 alt="Brown Sugar Milk Tea"/>
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            <div className="px-3 py-7 bg-orange-100">
+                <div className="font-black text-base mb-2 text-center uppercase">Brown Sugar Milk Tea</div>
+                <p className="text-stone-500 text-base text-center font-light text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+                <p className="text-gray-700 text-base text-center p-4">
+                    $6.25
                 </p>
             </div>
             </div>
@@ -98,10 +113,13 @@ export default function Tiles() {
                 height={234}
                 className="w-full"
                 alt="Brown Sugar Milk Tea"/>
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            <div className="px-3 py-7 bg-orange-100">
+                <div className="font-black text-base mb-2 text-center uppercase">Brown Sugar Milk Tea</div>
+                <p className="text-stone-500 text-base text-center font-light text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+                <p className="text-gray-700 text-base text-center p-4">
+                    $6.25
                 </p>
             </div>
             </div>
@@ -112,10 +130,13 @@ export default function Tiles() {
                 height={234}
                 className="w-full"
                 alt="Brown Sugar Milk Tea"/>
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            <div className="px-3 py-7 bg-orange-100">
+                <div className="font-black text-base mb-2 text-center uppercase">Brown Sugar Milk Tea</div>
+                <p className="text-stone-500 text-base text-center font-light text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+                <p className="text-gray-700 text-base text-center p-4">
+                    $6.25
                 </p>
             </div>
             </div>
@@ -126,10 +147,30 @@ export default function Tiles() {
                 height={234}
                 className="w-full"
                 alt="Brown Sugar Milk Tea"/>
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            <div className="px-3 py-7 bg-orange-100">
+                <div className="font-black text-base mb-2 text-center uppercase">Brown Sugar Milk Tea</div>
+                <p className="text-stone-500 text-base text-center font-light text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+                <p className="text-gray-700 text-base text-center p-4">
+                    $6.25
+                </p>
+            </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <Image
+                src="/images/drinks/bs-mt.webp"
+                width={234}
+                height={234}
+                className="w-full"
+                alt="Brown Sugar Milk Tea"/>
+            <div className="px-3 py-7 bg-orange-100">
+                <div className="font-black text-base mb-2 text-center uppercase">Brown Sugar Milk Tea</div>
+                <p className="text-stone-500 text-base text-center font-light text-base">
+                    Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.
+                </p>
+                <p className="text-gray-700 text-base text-center p-4">
+                    $6.25
                 </p>
             </div>
             </div>
