5eec0217bdfab7b0cee87a35a8c36c27e42d8c3f
Nicole Mak <nicolexuemak@gmail.com>
2025-04-15 15:16:47 -0700

    changed image naming to imageUrl for json

--- src/components/BobaTile.tsx
+++ src/components/BobaTile.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-export default function BobaTile({ Drink Name, Image, Price, Description }) {
+export default function BobaTile({ drinkName, imageUrl, price, description }) {
     return (
         <div className="bobatile">
             <Image
-                src={Image}
+                src={imageUrl}
                 width={234}
                 height={234}
                 className="w-full"
-                alt={Drink Name}
+                alt={drinkName}
             />
             <div className="px-3 pb-4 pt-4 bg-orange-100">
-                <div className="font-black text-base mb-2 text-center uppercase">{Drink Name}</div>
+                <div className="font-black text-base mb-2 text-center uppercase">{drinkName}</div>
                 <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
-                    {Description}
+                    {description}
                 </p>
                 <p className="text-gray-700 text-base text-center pt-6">
-                    {Price}
+                    {price}
                 </p>
             </div>
         </div>
