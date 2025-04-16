3e3057dd55e0663868a43a982bbfd22d34df2212
Nicole Mak <nicolexuemak@gmail.com>
2025-04-16 10:01:41 -0700

    removed conditional rendering of imageUrl since I've already hard coded the data

--- src/components/BobaTile.tsx
+++ src/components/BobaTile.tsx
@@ -11,16 +11,13 @@ interface BobaTileProps {
 export default function BobaTile({ drinkName, imageUrl, price, description }: BobaTileProps) {
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            {imageUrl && (
-                <Image
-                    src={imageUrl}
-                    width={234}
-                    height={234}
-                    className="w-full"
-                    alt={drinkName}
-                />
-            )
-            }
+            <Image
+                src={imageUrl}
+                width={234}
+                height={234}
+                className="w-full"
+                alt={drinkName}
+            />
             <div className="px-3 pb-4 pt-4 bg-orange-100">
                 <div className="font-black text-base mb-2 text-center uppercase">{drinkName}</div>
                 <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
