9db2f72c2fa5e04bfa3bdf0ebaeee9d3ba2d13ef
Nicole Mak <nicolexuemak@gmail.com>
2025-04-16 14:20:26 -0700

    Added 'test': 'jest' script so that Jest can test. Created first attempt of BobaTile test file

---
+++ src/components/BobaTile.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import BobaTile from "./BobaTile";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
+
+const mockProps = {
+    drinkName: 'Brown Sugar Milk Tea',
+    imageUrl: './public/images/drinks/bs-mt.webp',
+    price: 6.25,
+    description: 'Black milk tea made with non-dairy creamer, sweetened and drizzled with caramelized brown sugar.'
+};
+
+describe(BobaTile, () => {
+
+    it("renders drink image, drink name, drink description, and drink price", () => {
+        render(<BobaTile {...mockProps}/>);
+        expect(screen.getByText(mockProps.drinkName)).toBeInTheDocument();
+        expect(screen.getByText(mockProps.description)).toBeInTheDocument();
+        expect(screen.getByText(mockProps.price.toString())).toBeInTheDocument();
+        expect(screen.getByAltText(mockProps.drinkName)).toBeInTheDocument();
+    })
+})
