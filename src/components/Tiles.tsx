4b73dcf44238ae5cb74e024b6896aa131d8be80e
Nicole Mak <nicolexuemak@gmail.com>
2025-04-17 11:59:34 -0700

    Component/bobatile (#5)
    * Drafted tile features with card components
    
    * Added drink details for the first drink, Brown Sugar Milk Tea
    
    * Added styling to the first card, Brown Sugar Milk Tea, to get it as close to the actual website.
    
    * Applied uniform styling to all card components for the milk tea section
    
    * Replaced placeholder text with actual names, details, prices, and photos
    
    * Got padding to be consistent throughout the top 4 cards so that each element of the bottom portion of the card component is aligned nicely
    
    * Reduced bottom padding of price content
    
    * Applied updated card content padding to the rest of the card components
    
    * fixed react hydration error bug by making sure the nesting of HTML tags are correct and clean
    
    * Created new BobaTile component and changed naming convention of keys in json data to be camelCase
    
    * changed image naming to imageUrl for json
    
    * Added content to BobaTile component
    
    * fixed implicity 'any' error by defining the types of the function's parameters
    
    * removed conditional rendering of imageUrl since I've already hard coded the data
    
    * installed jest and created new BobaTile test file
    
    * installed Jest with TypeScript type definitions
    
    * installed React Testing Library with TypeScript and Jest DOM
    
    * Added 'test': 'jest' script so that Jest can test. Created first attempt of BobaTile test file
    
    * Don't need babel
    
    * Removed React import from BobaTile test
    
    * Installed prettier, changed indent spacing from 4 to 2
    
    * Added 4 separate assertions and tests for each prop

---
+++ src/components/Tiles.tsx
@@ -0,0 +1,192 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function Tiles() {
+  return (
+    <div className="grid grid-cols-4 gap-6 p-50">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/bs-mt.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Brown Sugar Milk Tea"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Brown Sugar Milk Tea
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Black milk tea made with non-dairy creamer, sweetened and drizzled
+            with caramelized brown sugar.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/classic-mt.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Classic Milk Tea"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Classic Milk Tea
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Plain and simple black milk tea made with non-dairy creamer.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/jasmine-mt.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Jasmine Milk Tea"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Jasmine Milk Tea
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Green tea brewed with a slightly floral aroma made with our
+            non-dairy creamer house milk.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/thai-kick.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Thai Kick"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Thai Kick
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Thai milk tea with a hint of cold brew coffee.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/taro-mt.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Taro Milk Tea"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Taro Milk Tea
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Slightly floral and bitter green milk tea paired with taro.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/wintermelon-mt.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Wintermelon Milk Tea"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Wintermelon Milk Tea
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Caramel-like flavor of wintermelon makes a sweet milk tea.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/honeydew-mt.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Honeydew Milk Tea"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Honeydew Milk Tea
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Green milk tea with honeydew.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/rose-mt.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Rose Milk Tea"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Rose Milk Tea
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Strong floral milk tea complimented with rose.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/tea-float.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Thai Float"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Thai Float
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Ice cream mixed with strong tea makes an extra tasty milk tea.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <Image
+          src="/images/drinks/thai-mt.webp"
+          width={234}
+          height={234}
+          className="w-full"
+          alt="Thai Milk Tea"
+        />
+        <div className="px-3 pb-4 pt-4 bg-orange-100">
+          <div className="font-black text-base mb-2 text-center uppercase">
+            Thai Milk Tea
+          </div>
+          <p className="text-stone-500 text-base text-center font-light text-base p-2 w-50 h-30">
+            Our strongly-brewed Thai milk tea mixed with our non-dairy house
+            milk is a classic favorite.
+          </p>
+          <p className="text-gray-700 text-base text-center pt-6">$6.25</p>
+        </div>
+      </div>
+    </div>
+  );
+}
