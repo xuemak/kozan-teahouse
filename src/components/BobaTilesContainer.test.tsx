320dd968c084f44a076f5743a15c025f42e19387
Nicole Mak <nicolexuemak@gmail.com>
2025-04-18 10:01:32 -0700

    Created BobaTilesContainer test file with imports

---
+++ src/components/BobaTilesContainer.test.tsx
@@ -0,0 +1,4 @@
+import BobaTilesContainer from './BobaTilesContainer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import bobaMenuJson from '../data/bobaMenu.json';
