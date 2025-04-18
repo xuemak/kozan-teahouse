5270136b286d48b37e3e2eb7e3fffb3c00501c28
Nicole Mak <nicolexuemak@gmail.com>
2025-04-18 11:21:56 -0700

    removed uneccessary import of json data since it was already declared in the jest.mock function

--- src/components/BobaTilesContainer.test.tsx
+++ src/components/BobaTilesContainer.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import BobaTilesContainer from './BobaTilesContainer';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import bobaMenuJson from '../data/bobaMenu.json';
 
 jest.mock('../data/bobaMenu.json', () => ({
   sections: [
