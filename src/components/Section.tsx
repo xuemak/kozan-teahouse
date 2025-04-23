67a8b6032561e36389e4b09861d9284894e73520
Nicole Mak <nicolexuemak@gmail.com>
2025-04-23 09:50:02 -0700

    fixed awkward white spacing on the bottom of card component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -13,7 +13,7 @@ export default function Section({ sectionName, drinkItems }: SectionProps) {
       <h1 className="font-black text-5xl mb-2 text-center uppercase">
         {sectionName}
       </h1>
-      <div className="grid grid-cols-2 gap-6 p-4 md:p-10 md:grid-cols-3 lg:grid-cols-4 lg:max-w-screen-lg lg:mx-auto">
+      <div className="grid grid-cols-2 gap-5 p-4 md:p-10 md:grid-cols-3 lg:grid-cols-4 lg:max-w-screen-lg lg:mx-auto">
         {drinkItems.map((drink) => (
           <BobaTile
             key={drink.drinkName}
