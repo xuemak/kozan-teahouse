22e456237fc8664a9e92ad9417271d71570f41c5
Nicole Mak <nicolexuemak@gmail.com>
2025-04-24 15:52:54 -0700

    positioned buttons to be justified with the content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,33 +12,49 @@ export default function Header() {
           'linear-gradient(54deg, rgba(134, 78, 149, 1) 0%, rgba(178, 127, 156, 1) 26%, rgba(139, 82, 149, 1) 63%, rgba(152, 98, 152, 1) 78%, rgba(178, 127, 156, 1) 100%, rgba(134, 78, 149, 1) 100%)',
       }}
     >
-      <div className="flex flex-col items-center">
-        <Image
-          src="/kozanlogo_fulltext.webp"
-          alt="Kozan Tea House Full Text Logo"
-          width={252}
-          height={87}
-        />
-        <ul className="flex space-x-7 mt-5 uppercase font-bold tracking-wider">
-          <li>
-            <Link href="https://www.instagram.com/kozanteahouse/?hl=en">
-              Instagram
-            </Link>
-          </li>
-          <li>
-            <Link href="https://squareup.com/gift/E2SHH2PPB06TF/order">
-              E-Gift Card
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="absolute top-4 right-4 space-x-4">
-        <button>
-          <Image src="/user_icon.png" alt="user icon" width={25} height={25} />
-        </button>
-        <button>
-          <Image src="/cart.png" alt="cart icon" width={25} height={25} />
-        </button>
+      <div
+        style={{
+          maxWidth: '930px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          position: 'relative',
+        }}
+      >
+        <div className="flex items-center justify-between">
+          <div className="flex-grow flex flex-col items-center">
+            <Image
+              src="/kozanlogo_fulltext.webp"
+              alt="Kozan Tea House Full Text Logo"
+              width={252}
+              height={87}
+            />
+            <ul className="flex space-x-7 mt-5 uppercase font-bold tracking-wider">
+              <li>
+                <Link href="https://www.instagram.com/kozanteahouse/?hl=en">
+                  Instagram
+                </Link>
+              </li>
+              <li>
+                <Link href="https://squareup.com/gift/E2SHH2PPB06TF/order">
+                  E-Gift Card
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="absolute top-0 right-0 flex space-x-4">
+            <button className="mx-2">
+              <Image
+                src="/user_icon.png"
+                alt="user icon"
+                width={25}
+                height={25}
+              />
+            </button>
+            <button className="mx-2">
+              <Image src="/cart.png" alt="cart icon" width={25} height={25} />
+            </button>
+          </div>
+        </div>
       </div>
     </header>
   );
