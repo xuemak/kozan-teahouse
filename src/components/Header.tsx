d64f3297bbd6c5dbc8757bef67072dfdda2c16cf
Nicole Mak <nicolexuemak@gmail.com>
2025-04-24 10:39:57 -0700

    added custom gradient background that closely matches to the og website

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,14 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header>
+    <header
+      className="py-4"
+      style={{
+        background: '#864E95',
+        backgroundImage:
+          'linear-gradient(54deg, rgba(134, 78, 149, 1) 0%, rgba(178, 127, 156, 1) 26%, rgba(139, 82, 149, 1) 63%, rgba(152, 98, 152, 1) 78%, rgba(178, 127, 156, 1) 100%, rgba(134, 78, 149, 1) 100%)',
+      }}
+    >
       <div>
         <Image
           src="/kozanlogo_fulltext.webp"
