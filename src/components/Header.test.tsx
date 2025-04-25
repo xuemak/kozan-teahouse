2501e91400d67e485fc5c9118dfeb16347410ac7
Nicole Mak <nicolexuemak@gmail.com>
2025-04-25 09:31:21 -0700

    drafted Header test file

---
+++ src/components/Header.test.tsx
@@ -0,0 +1,7 @@
+import Header from './Header';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe(Header, () => {
+  it('renders Kozan Tea House full logo', () => {});
+});
